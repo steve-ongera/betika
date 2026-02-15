6252621004b345edde12166bd15d201a29239109
steve-ongera <gadafisteve001@gmail.com>
2026-02-16 00:44:31 +0300

    made updates

--- betika/settings.py
+++ betika/settings.py
@@ -94,23 +94,49 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Africa/Nairobi'
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_URL = 'static/'
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# M-Pesa Configuration
+MPESA_ENVIRONMENT = 'sandbox'  # Change to 'production' for live
+MPESA_CONSUMER_KEY = 'your_consumer_key'
+MPESA_CONSUMER_SECRET = 'your_consumer_secret'
+MPESA_SHORTCODE = 'your_shortcode'
+MPESA_PASSKEY = 'your_passkey'
+MPESA_CALLBACK_URL = 'https://yourdomain.com/mpesa/callback/'
+
+# For B2C (Withdrawals)
+MPESA_INITIATOR_NAME = 'your_initiator_name'
+MPESA_SECURITY_CREDENTIAL = 'your_security_credential'
+MPESA_B2C_TIMEOUT_URL = 'https://yourdomain.com/mpesa/b2c/timeout/'
+MPESA_B2C_RESULT_URL = 'https://yourdomain.com/mpesa/b2c/result/'
+
+# Session Configuration
+SESSION_COOKIE_AGE = 86400  # 24 hours
+SESSION_SAVE_EVERY_REQUEST = True
+
+# Security Settings (for production)
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Login URL
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/game/'
+LOGOUT_REDIRECT_URL = '/login/'
